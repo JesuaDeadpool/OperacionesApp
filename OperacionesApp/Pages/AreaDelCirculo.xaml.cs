bcb7f1b309e020f2b6b1f447aa281fab5aeb4c05
Jesua <jesuaduron@gmail.com>
2025-03-09 12:46:36 -0600

    validacion y integrar la formula del area del circulo

--- OperacionesApp/Pages/AreaDelCirculo.xaml.cs
+++ OperacionesApp/Pages/AreaDelCirculo.xaml.cs
@@ -6,4 +6,31 @@ public partial class AreaDelCirculo : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnCalCir_Clicked(object sender, EventArgs e)
+    {
+        if (txtRadio.Text is null || txtRadio.Text == "")
+        {
+            DisplayAlert("ADVERTENCIA", "Radio esta en blanco", "Aceptar");
+            txtRadio.BackgroundColor = Colors.Red;
+            txtRadio.Focus();
+        }
+        else
+        {
+            double radio = 0;
+            double area = 0;
+
+            radio = Convert.ToDouble(txtRadio.Text);
+
+            area = Math.PI * Math.Pow(radio, 2);
+
+            txtResultado.Text = area.ToString() + "cm2"; 
+        }
+
+    }
+
+    private void BtnLimpiar_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
